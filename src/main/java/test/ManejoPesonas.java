adfcd8958c13ad0b99dbaa9a9c1accc75d33079f
HerreraFelipe <herrerafelipe.093@gmail.com>
2021-05-21 13:26:27 -0300

    Se modifico ManejoPersonas.java

--- src/main/java/test/ManejoPesonas.java
+++ src/main/java/test/ManejoPesonas.java
@@ -1,21 +1,59 @@
 package test;
 
+import datos.Conexion;
 import datos.PersonaJDBC;
 import domain.Persona;
-import java.util.List;
-
+import java.sql.*;
 
 public class ManejoPesonas {
     
     public static void main(String[] args) {
         
-        //Prueba de Personas
-        PersonaJDBC personaJDBC = new PersonaJDBC();
+        // Conexion Transaccional
+        Connection conexion = null;
         
-        List<Persona> listaPersonas = personaJDBC.select();
-        
-        for (Persona persona : listaPersonas) {
-            System.out.println("Persona: " + persona);
+        try {
+            conexion = Conexion.getConnection();
+            
+            if (conexion.getAutoCommit()) { // si AutoCommit = true
+                conexion.setAutoCommit(false); 
+            }
+            
+            // Establecer conexion Transaccional
+            PersonaJDBC personaJDBC = new PersonaJDBC(conexion);
+            
+            // sentencias a ejecutar dentro de la transaccion
+            
+            // Update
+            Persona cambioPersona = new Persona();
+            cambioPersona.setId_persona(2);
+            cambioPersona.setNombre("Andres Felipe");
+            cambioPersona.setApellido("Herrera");
+            cambioPersona.setApellido("fherrera@email.com");
+            cambioPersona.setTelefono("12347665");
+            
+            personaJDBC.update(cambioPersona); // Actualiza obj en la BD
+            
+            // Insert
+            Persona nuevaPersona = new Persona();
+            nuevaPersona.setNombre("Carlos");
+            nuevaPersona.setApellido("Gomez");
+            
+            personaJDBC.insert(nuevaPersona);
+            
+            // Commit de la Transaccion
+            conexion.commit();
+        } 
+        catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+            System.out.println("Entramos al rollback");
+            
+            try {
+                conexion.rollback();
+            } 
+            catch (SQLException ex1) {
+                ex1.printStackTrace(System.out);
+            }
         }
         
     }
