cb8285571e461b7ba6f5147530c7686ce59429a0
HerreraFelipe <herrerafelipe.093@gmail.com>
2021-05-23 01:22:22 -0300

    Se modifico ManejoUsuarios.java

--- src/main/java/test/ManejoUsuarios.java
+++ src/main/java/test/ManejoUsuarios.java
@@ -1,34 +1,62 @@
 package test;
 
-import datos.*;
-import domain.*;
-import java.util.*;
+import datos.Conexion;
+import datos.UsuarioJDBC;
+import domain.Usuario;
+import java.sql.*;
 
 public class ManejoUsuarios {
 
     public static void main(String[] args) {
         
+        // Conexion transaccional
+        Connection conexion = null;
         
-        UsuarioJDBC usuarioJDBC = new UsuarioJDBC();
-
-        List<Usuario> listaUsuarios = usuarioJDBC.select();
-
-        // Recorremos con un for
-        for (Usuario usuario : listaUsuarios) {
-            System.out.println("Usuarios: " + usuario);
+        try {
+            conexion = Conexion.getConnection();
+            
+            if ( conexion.getAutoCommit() ) {
+                conexion.setAutoCommit(false);
+            }
+            
+            // Establecer conexion
+            UsuarioJDBC usuarioJDBC = new UsuarioJDBC(conexion);
+            
+            // Sentencias SQL
+            
+            // UPDATE
+            Usuario cambioUsuario = new Usuario();
+            
+            cambioUsuario.setId_usuario(3);
+            cambioUsuario.setUsuario("EdwinHerrera");
+            cambioUsuario.setPassword("111abc");
+            
+            usuarioJDBC.update(cambioUsuario); 
+            
+            // INSERT
+            Usuario nuevoUsuario = new Usuario();
+            
+            nuevoUsuario.setUsuario("Estela");
+            nuevoUsuario.setPassword("123abc");
+            
+            usuarioJDBC.insert(nuevoUsuario);
+            
+            // Commit de la transaccion
+            conexion.commit();
+            System.out.println("Se ha hecho el Commit de la transaccion.");
+            
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+            System.out.println("Entramos al rollback");
+            
+            try {
+                conexion.rollback();
+            }
+            catch (SQLException ex1) {
+                ex1.printStackTrace(System.out);
+            }
         }
         
-        // Insertar nuevo usuario
-//        Usuario user = new Usuario("carlos ", "1234");
-//        usuarioJDBC.insert(user);
-        
-        
-        // Modificar usuario existente
-//        Usuario user2 = new Usuario(4, "carlos", "456");
-//        usuarioJDBC.update(user2);
-
-        // Eliminar usuario
-        usuarioJDBC.delete(new Usuario(4));
     }
 
 }
