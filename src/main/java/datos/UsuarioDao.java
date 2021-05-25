1b289492d89e5717545e78def3812431b47a1d19
HerreraFelipe <herrerafelipe.093@gmail.com>
2021-05-24 23:27:08 -0300

    Se modifico UsuarioDao

--- src/main/java/datos/UsuarioDao.java
+++ src/main/java/datos/UsuarioDao.java
@@ -1,15 +1,16 @@
 package datos;
 
+import java.sql.SQLException;
 import java.util.List;
 import domain.UsuarioDTO;
 
 public interface UsuarioDao {
 
-	public List<UsuarioDTO> select();
+	public List<UsuarioDTO> select() throws SQLException;
 	
-	public int insert(UsuarioDTO usuario);
+	public int insert(UsuarioDTO usuario) throws SQLException;
 	
-	public int update(UsuarioDTO usuario);
+	public int update(UsuarioDTO usuario) throws SQLException;
 	
-	public int delete(UsuarioDTO usuario);
+	public int delete(UsuarioDTO usuario) throws SQLException;
 }
